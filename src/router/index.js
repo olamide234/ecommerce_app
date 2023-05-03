7135ab80bcdf60a4eec42543f157842be190e230
Olamide Tofade <tofade.olamide2@gmail.com>
2023-05-03 02:30:48 +0100

    feat: home and past order pages route

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,8 @@
 /* eslint-disable */
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '../views/Home.vue'
 import Products from '../views/Products.vue'
+import PastOrders from '../views/PastOrders.vue'
 
 const routes = [
   {
@@ -16,7 +17,12 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: Products
-  }
+  },
+  {
+    path: '/past-orders',
+    name: 'PastOrders',
+    component: PastOrders
+  },
 ]
 
 const router = createRouter({
